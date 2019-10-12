49f55e2a2afa5ac835d483e60a0696f5fc5c1a87
Levi Szilveszter <l3vi.sylvester@gmail.com>
2019-10-12 22:05:00 +0300

    simplified Type, CommandLine has exit & checks

--- src/main/java/w00_typeModified/CommandLine.java
+++ src/main/java/w00_typeModified/CommandLine.java
@@ -3,52 +3,96 @@ package w00_typeModified;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import static w00_typeModified.Type.type;
 
 // tema: pt fisiere .txt date ca parametri de intrare, sa se tipareasca continutul lor
 // this CommandLine class tries to simulate the console
-// type command currently accepts max 5 file paths separated with space (but it can support easily even more)
+
 public class CommandLine {
 
     public static void main(String[] args) throws IOException {
-
-        System.out.print(System.getProperty("user.dir") + "\\");
-
         Scanner scanner = new Scanner(System.in);
-        String commandLine = scanner.nextLine();
 
-        String[] command = commandLine.split(" ");
-        int numberOfFilePaths = command.length - 1;
-
-        try {
-            if (command[0].equals("type")) {
-                switch (numberOfFilePaths) {
-                    case 1:
-                        type(command[1]);
+        boolean exit = false;
+        while (!exit) {
+            System.out.println();
+            System.out.print(System.getProperty("user.dir") + "\\");
+            String commandLine = scanner.nextLine();
+            try {
+                String[] commands = commandLine.split(" ");
+                String[] files = Arrays.copyOfRange(commands, 1, commands.length, String[].class);
+                switch (commands[0]) {
+                    //now we only have type and exit but switch makes sense if other methods are added
+                    case "type":
+                        if (files.length != 0) {
+                            type(files);
+                        } else {
+                            System.out.println("no files specified");
+                        }
                         break;
-                    case 2:
-                        type(command[1], command[2]);
-                        break;
-                    case 3:
-                        type(command[1], command[2], command[3]);
+                    case "exit":
+                        exit = true;
                         break;
                     default:
-                        System.out.println("not a valid command");
+                        System.out.println("can't recognize command");
                 }
-            } else {
+            } catch (FileNotFoundException f) {
+                System.out.println("file not found");
+            } catch (IllegalArgumentException i) {
                 System.out.println("not a valid command");
             }
-        } catch (FileNotFoundException f) {
-            System.out.println("file not found");
         }
-        /* sample input:
-type file.txt file1.txt file2.txt */
+        scanner.close();
+    }
+}
+/** sample input:
+type file.txt file1.txt file2.txt
+*/
 
-        //toDo: regex; build a method that increments type method's accepted file paths to as many as entered
+//toDo: 1.what could i do to continue printing next file after file not found?; 2.regex
 
-        // Commented but worth to mention:
+
+// Commented but worth to mention:
+
+//        int numberOfFilePaths = commands.length - 1;
+
+//        for(int i=0; i<numberOfFilePaths;i++) {
+//            type(commands[i+1]);
+//        }
+
+//                if (commands[0].equals("type")) {
+//                    type(files);
+//                } else if (commands[0].equals("exit")) {
+////                    break;
+//                    exit = true;
+//                } else {
+//                    System.out.println("can't recognize command");
+//                }
+
+//        try {
+//            if (commands[0].equals("type")) {
+//                switch (files.length) {
+//                    case 1:
+//                        type(commands[1]);
+//                        break;
+//                    case 2:
+//                        type(commands[1], commands[2]);
+//                        break;
+//                    case 3:
+//                        type(commands[1], commands[2], commands[3]);
+//                        break;
+//                    default:
+//                        System.out.println("not a valid command");
+//                }
+//            } else {
+//                System.out.println("not a valid command");
+//            }
+//        } catch (FileNotFoundException f) {
+//            System.out.println("file not found");
+//        }
 
 //        File currentDirFile = new File(".");
 //        String helper = currentDirFile.getAbsolutePath();
@@ -57,19 +101,17 @@ type file.txt file1.txt file2.txt */
 
 //        int numberOfPaths = commandLine.split(scanner.delimiter().pattern()).length-1;
 
-//        String command = scanner.next();
+//        String commands = scanner.next();
 //        String file = scanner.next();
 //        String file1 = scanner.next();
 //        String file2 = scanner.next();
 
 //                if (numberOfFilePaths == 1) {
-//                    type(command[1]);
+//                    type(commands[1]);
 //                } else if (numberOfFilePaths == 2) {
-//                    type(command[1], command[2]);
+//                    type(commands[1], commands[2]);
 //                } else if (numberOfFilePaths == 3) {
-//                    type(command[1], command[2], command[3]);
+//                    type(commands[1], commands[2], commands[3]);
 //                } else {
-//                    System.out.println("not a valid command");
+//                    System.out.println("not a valid commands");
 //                }
-    }
-}
--- src/main/java/w00_typeModified/Type.java
+++ src/main/java/w00_typeModified/Type.java
@@ -7,52 +7,20 @@ import java.io.IOException;
 public class Type {
 
     public static void type(String file) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(file));
         System.out.println();
         System.out.println(file + ":");
-        for (String line; (line = br.readLine()) != null; ) {
+        BufferedReader toBuffer = new BufferedReader(new FileReader(file));
+        for (String line; (line = toBuffer.readLine()) != null; ) {
             System.out.println(line);
         }
     }
 
-    public static void type(String file, String otherFile) throws IOException {
-        type(file);
-        type(otherFile);
-//        BufferedReader br = new BufferedReader(new FileReader(file));
-//        BufferedReader br1 = new BufferedReader(new FileReader(otherFile));
-//        System.out.println();
-//        System.out.println(file + ":");
-//        for (String line; (line = br.readLine()) != null; ) {
-//            System.out.println(line);
-//        }
-//        System.out.println();
-//        System.out.println(otherFile + ":");
-//        for (String line; (line = br1.readLine()) != null; ) {
-//            System.out.println(line);
-//        }
+    //apparently we can use String[] or String... as parameters with the same result
+    //is there ANY difference between them?
+    public static void type(String... files) throws IOException {
+        for (String file : files) {
+            type(file);
+        }
     }
-
-    public static void type(String file, String secondFile, String thirdFile) throws IOException {
-        type(file);
-        type(secondFile);
-        type(thirdFile);
-    }
-
-    public static void type(String file, String secondFile, String thirdFile, String forthFile) throws IOException {
-        type(file);
-        type(secondFile);
-        type(thirdFile);
-        type(forthFile);
-    }
-
-    public static void type(String file, String secondFile, String thirdFile, String forthFile, String fifthFile)
-            throws IOException {
-        type(file);
-        type(secondFile);
-        type(thirdFile);
-        type(forthFile);
-        type(fifthFile);
-    }
-    // and so on
 }
 
